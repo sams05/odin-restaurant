b7ac3e921aac03be3aed6dcf5aab1e4968963baf
Samuel Shen <sshen05@terpmail.umd.edu>
2023-08-24 01:12:43 -0400

    Implement tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import loadPage from './page-load';
 import './style.css';
+import createHomePage from './home';
+import createMenuPage from './menu';
+import createContactPage from './contact';
 
 (function() {
+    function switchTab(e) {
+        const main = document.querySelector('.main');
+        const id = e.target.id;
+        let newMain;
+        switch(id) {
+            case 'nav-home':
+                newMain = createHomePage();
+                break;
+            case 'nav-menu':
+                newMain = createMenuPage();
+                break;
+            case 'nav-contact':
+                newMain = createContactPage();
+                break;
+            default:
+                throw new Error('Invalid tab id');
+        }
+        main.replaceWith(newMain);
+    }
+    
     loadPage();
+    const tabs = document.querySelectorAll('.nav-btn');
+    for(let tab of tabs) {
+        tab.addEventListener('click', switchTab);
+    }
 })();
